930e06f687b3fcb9869aaf1c698bcfcb4a38dee3
gowthami167 <69572396+gowthami167@users.noreply.github.com>
2021-03-19 16:41:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    public class Sampleclass
+    {
+        public void Gmethod<T>(T a, T b)
+        {
+            Console.WriteLine("param 1:{0},a");
+            Console.WriteLine("param2:{0},b");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Generics method example**");
+            Sampleclass s = new Sampleclass();
+            s.Gmethod<int>(1, 2);
+            s.Gmethod("sujatha", "kerala");
+            Console.ReadLine();
+        }
+    }
+}
+
