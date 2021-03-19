e20483153d134c897403849fca833c3dc3f0a9a3
gowthami167 <69572396+gowthami167@users.noreply.github.com>
2021-03-19 16:26:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int add, sub, mul, div;
+            Console.WriteLine("enter a value");
+            int a=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter b value");
+            int b = int.Parse(Console.ReadLine());
+            add = a + b;
+            sub = a - b;
+            mul = a * b;
+            div=a/b;
+            Console.WriteLine("the result is:" + add);
+            Console.WriteLine("the result is:" + sub);
+            Console.WriteLine("the result is:" + mul);
+            Console.WriteLine("the result is:" + div);
+            Console.ReadLine();
+
+
+        }
+    }
+}
